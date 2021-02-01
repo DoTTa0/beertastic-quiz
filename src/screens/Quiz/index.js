1ebae351d187aed6a27426ffdb92c9b08d3aa9ae
Giovanni Dotta <giovanni.dotta0@gmail.com>
2021-01-31 22:22:49 -0300

    final da aula 5 Imersao alura- correçao 2

--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -119,7 +119,7 @@ function QuestionWidget({
               onSubmit();
               setIsQuestionSubmited(false);
               setSelectedAlternative(undefined);
-            }, 3 * 1000);
+            }, 1 * 1000);
           }}
         >
           {question.alternatives.map((alternative, alternativeIndex) => {
@@ -140,6 +140,7 @@ function QuestionWidget({
                   name={questionId}
                   onChange={() => setSelectedAlternative(alternativeIndex)}
                   type="radio"
+                  checked={isSelected}
                 />
                 {alternative}
               </Widget.Topic>
